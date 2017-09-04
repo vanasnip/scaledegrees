9e7f14d78495f0a5aaeb907f5b54144fab6b5272
popPop <poppop@loves-mbp.home>
2017-09-04 21:41:55 +0100

    Routing added, params transfering

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-  $scope.change = $scope.value;
+var app = angular.module('myApp', ["ngRoute"]);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when("/home", {
+      templateUrl: "home.html",
+      controller: "myCtrl"
+    })
+    .when("/keytest/:theKey/:keyArr", {
+      templateUrl: "keytest.html",
+      controller: "myCtrl"
+    })
+    .otherwise ({redirectTo:"/home"});
+
+});
+
+app.controller('myCtrl', function($scope, $location, $routeParams) {
+    
+    $scope.noteNames = ["a","b","c","d","e","f","g"];
+    $scope.accidentals = [{sym:"♯", state:false}, {sym:"♭", state:false}]
     $scope.degrees = {
       d1: {
         degree: "I",
-        degKey: 0
+        degKey: 0,
       },
       d2: {
         degree: "II",
@@ -35,66 +52,115 @@ app.controller('myCtrl', function($scope) {
     $scope.keys = [
       {
         keyName: "C",
-        notes: ["c","d","e","f","g","a","b"]
+        notes: ["c","d","e","f","g","a","b"],
+        index: 0
       },
       {
         keyName: "G",
-        notes: ["g","a","b","c","d","e","f♯"]
+        notes: ["g","a","b","c","d","e","f♯"],
+        index: 1
       },
       {
         keyName: "D",
-        notes: ["d","e","f♯","g","a","b","c♯"]
+        notes: ["d","e","f♯","g","a","b","c♯"],
+        index: 2
         //D, E, F♯, G, A, B, C♯
       },
       {
         keyName: "A",
-        notes: ["a","b","c♯","d","e","f♯","g♯"]
+        notes: ["a","b","c♯","d","e","f♯","g♯"],
+        index: 3
         //A, B, C♯, D, E, F♯, G♯
       },
       {
         keyName: "E",
-        notes: ["e","f♯","g♯","a","b","c♯","d♯"]
+        notes: ["e","f♯","g♯","a","b","c♯","d♯"],
+        index: 4
         //E, F♯, G♯, A, B, C♯, D♯
       },
       {
         keyName: "B",
-        notes: ["b","c♯","d♯","e","f♯","g♯","a♯"]
+        notes: ["b","c♯","d♯","e","f♯","g♯","a♯"],
+        index: 5
         //B, C♯, D♯, E, F♯, G♯, A♯
         //C♭, D♭, E♭, F♭, G♭, A♭, B♭
       },
       {
         keyName: "C♭",
-        notes: ["c♭","d♭","e♭","f♭","g♭","a♭","b♭"]
+        notes: ["c♭","d♭","e♭","f♭","g♭","a♭","b♭"],
+        index: 6
         //C♭, D♭, E♭, F♭, G♭, A♭, B♭
       },
       {
         keyName: "F♯",
-        notes: ["f♯","g♯","a♯","b","c♯","d♯","e♯"]
+        notes: ["f♯","g♯","a♯","b","c♯","d♯","e♯"],
+        index: 7
         //F♯, G♯, A♯, B, C♯, D♯, E♯
       },
       {
-        keyName: "G",
-        notes: ["g","a","b","c","d","e","f♯"]
+        keyName: "G♭",
+        notes: ["g♭","a♭","b♭","c♭","d♭","e♭","f"],
+        index: 8
       },
       {
-        keyName: "G",
-        notes: ["g","a","b","c","d","e","f♯"]
+        keyName: "D♭",
+        notes: ["d♭","e♭","f","g♭","a♭","b♭","c"],
+        index: 9
       },
       {
-        keyName: "G",
-        notes: ["g","a","b","c","d","e","f♯"]
+        keyName: "A♭",
+        notes: ["a♭","b♭","c♯","d♭","e♭","f","g"],
+        index: 10
       },
       {
-        keyName: "G",
-        notes: ["g","a","b","c","d","e","f♯"]
+        keyName: "E♭",
+        notes: ["e♭","f","g","a♭","b♭","c","d"],
+        index: 11
       },
       {
-        keyName: "G",
-        notes: ["g","a","b","c","d","e","f♯"]
+        keyName: "B♭",
+        notes: ["b♭","c","d","e♭","f","g","a"],
+        index: 12
+      },
+      {
+        keyName: "F",
+        notes: ["f","g","a","b♭","c","d","e"],
+        index: 13
       }
       
     ];
+     $scope.theKey = $routeParams.theKey;
+     $scope.keyArr = $routeParams.keyArr;
      $scope.myKeys = $scope.keys[0];
+     $scope.selectedKey = "?";
+     $scope.keyArray;
+
+     $scope.keyTest = function(){
+      if($scope.selectedKey != "?"){
+        $scope.keyArray = $scope.getKeyArray($scope.selectedKey);
+        $location.path("/keytest/" + $scope.selectedKey +"/"+$scope.keyArray);
+        
+        
+
+      }
+     }
+
+      $scope.getKeyArray = function(key){
+        console.log("testing " + key);
+        for(var i = 0; i < $scope.keys.length; i++){
+          if(key === $scope.keys[i].keyName){
+            return $scope.keys[i].notes;
+          }          
+        }
+      }
+     
+
+     $scope.selectKey = function(leKey){
+      $scope.selectedKey = leKey.keyName;
+        return $scope.selectedKey;
+     }
+
+
 });
 
 /*
