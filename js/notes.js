46c7b2ebf5635d5f96fa8dd227cd87b2f83e97ee
vanasnip <IMamenemuno@ArkGroup.local>
2017-09-26 12:43:45 +0100

    added notes for progress tracking feature

---
+++ js/notes.js
@@ -0,0 +1,15 @@
+var allHistory = ['session object','session object','session object'];
+var session = {
+	startTime: 1002929010000000,
+	keyDegreeAns: [{scale: 'major', key:'C', degree: 6, time: 10029290191910, correct: false, }, {key:'C', degree: 2, time: 10029290191910, correct: false}]
+	
+};
+
+/*
+var loadHistory = 
+for each session start 
+total time for session =  session.keyDegreeAns[session.keyDegreeAns.length].time - session.startTime
+
+***Show clock 
+** suggest showing clock in count down then red in negative
+*/
